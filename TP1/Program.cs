07f35823adda09c29c95d90a2baf3084621a66c3
LautaroDuarte <lautaroduarte1@gmail.com>
2022-05-29 03:03:25 -0300

    Com Final EMB

--- TP1/Program.cs
+++ TP1/Program.cs
@@ -10,86 +10,108 @@ namespace TP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Para comenzar a Ingresar los pasajeros en los taxis presiona una tecla");
-            Console.ReadKey();
+            Console.WriteLine("Sistema de Carga de Pasajeros");
+            Console.WriteLine("Menu de Opciones");
+            Console.WriteLine("1 - Carga de Datos");
+            Console.WriteLine("2 - Impresion de datos en pantalla");
+            Console.WriteLine("3 - Salir");
+            Console.WriteLine("Ingrese su opcion");
+            int opcion = int.Parse(Console.ReadLine());
             int[] pasajerosTaxi = new int[5];
-            for (int i = 1; i < 6; i++)
-            {
-                
-                try
-                {
-                    Console.WriteLine($"Ingrese cantidad de pasajeros en taxi N° {i}");
-                    pasajerosTaxi[i - 1] = int.Parse(Console.ReadLine());
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("El valor ingresado no es un numero intente nuevamente");
-                    pasajerosTaxi[i - 1] = int.Parse(Console.ReadLine());
-                }
-                
-            }
-            Console.WriteLine("Hemos finalizado con la carga de pasajeros en la categoria Taxis");
-            Console.WriteLine("Para comenzar a Ingresar los pasajeros en los Omnibus presiona Enter");
-            Console.ReadKey();
             int[] pasajerosOmnibus = new int[5];
-            for (int i = 1; i < 6; i++)
+            while (opcion != 3)
             {
-                
-                try
+                switch (opcion)
                 {
-                    Console.WriteLine($"Ingrese cantidad de pasajeros en Omnibus N° {i}");
-                    pasajerosOmnibus[i - 1] = int.Parse(Console.ReadLine());
+                    case 1:
+
+                        for (int i = 1; i < 6; i++)
+                        {
+
+                            try
+                            {
+                                Console.WriteLine($"Ingrese cantidad de pasajeros en taxi N° {i}");
+                                pasajerosTaxi[i - 1] = int.Parse(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("El valor ingresado no es un numero intente nuevamente");
+                                pasajerosTaxi[i - 1] = int.Parse(Console.ReadLine());
+                            }
+
+                        }
+                        Console.WriteLine("Hemos finalizado con la carga de pasajeros en la categoria Taxis");
+                        Console.WriteLine("Continuamos con los Omnibus");
+                        
+
+                        for (int i = 1; i < 6; i++)
+                        {
+
+                            try
+                            {
+                                Console.WriteLine($"Ingrese cantidad de pasajeros en Omnibus N° {i}");
+                                pasajerosOmnibus[i - 1] = int.Parse(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("El valor ingresado no es un numero intente nuevamente");
+                                pasajerosOmnibus[i - 1] = int.Parse(Console.ReadLine());
+
+                            }
+                        }
+                        Console.WriteLine("Hemos finalizado con la carga de pasajeros en la categoria Omnibus");
+                        break;
+                    case 2:
+                        
+                        List<Taxi> pasajerosTaxis = new List<Taxi>
+                     {
+                     new Taxi(pasajerosTaxi[0]),
+                     new Taxi(pasajerosTaxi[1]),
+                     new Taxi(pasajerosTaxi[2]),
+                     new Taxi(pasajerosTaxi[3]),
+                     new Taxi(pasajerosTaxi[4]),
+
+                     };
+
+
+                        int f = 0;
+                        foreach (Taxi var in pasajerosTaxis)
+                        {
+                            f++;
+                            Console.WriteLine("El taxi N°{0} lleva {1} pasajero/s", f, var.Pasajeros);
+
+                        }
+
+                        List<Omnibus> pasajerosOmnibuss = new List<Omnibus>
+                    {
+                     new Omnibus(pasajerosOmnibus[0]),
+                     new Omnibus(pasajerosOmnibus[1]),
+                     new Omnibus(pasajerosOmnibus[2]),
+                     new Omnibus(pasajerosOmnibus[3]),
+                     new Omnibus(pasajerosOmnibus[4]),
+
+                    };
+                        Console.WriteLine("------------------------------------------------------------------------------");
+                        int d = 0;
+                        foreach (Omnibus var in pasajerosOmnibuss)
+                        {
+
+                            d++;
+                            Console.WriteLine("El Omnibus N°{0} lleva {1} pasajero/s", d, var.Pasajeros);
+
+                        }
+                        break;
+                       
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("El valor ingresado no es un numero intente nuevamente");
-                    pasajerosOmnibus[i - 1] = int.Parse(Console.ReadLine());
-                    
-                }
+                Console.WriteLine("Sistema de Carga de Pasajeros");
+                Console.WriteLine("Menu de Opciones");
+                Console.WriteLine("1 - Carga de Datos (Los datos sobreescribiran a los cargados previamente");
+                Console.WriteLine("2 - Impresion de datos en pantalla");
+                Console.WriteLine("3 - Salir");
+                Console.WriteLine("Ingrese su opcion");
+                opcion = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine("Hemos finalizado con la carga de pasajeros en la categoria Omnibus");
-            Console.WriteLine("Si queres ver el resumen de tu carga de datos presiona una tecla");
-            Console.ReadKey();
-            List<Taxi> pasajerosTaxis = new List<Taxi>
-            {
-                new Taxi(pasajerosTaxi[0]),
-                new Taxi(pasajerosTaxi[1]),
-                new Taxi(pasajerosTaxi[2]),
-                new Taxi(pasajerosTaxi[3]),
-                new Taxi(pasajerosTaxi[4]),
-
-            };
-
-           
-            int f = 0;
-            foreach (Taxi var in pasajerosTaxis)
-            {
-                
-                f++;
-                Console.WriteLine("El taxi N°{0} lleva {1} pasajero/s", f,var.Pasajeros);
-                
-                
-            }
-
-            List<Omnibus> pasajerosOmnibuss = new List<Omnibus>
-            {
-                new Omnibus(pasajerosOmnibus[0]),
-                new Omnibus(pasajerosOmnibus[1]),
-                new Omnibus(pasajerosOmnibus[2]),
-                new Omnibus(pasajerosOmnibus[3]),
-                new Omnibus(pasajerosOmnibus[4]),
-
-            };
-            Console.WriteLine("------------------------------------------------------------------------------");
-            int d = 0;
-            foreach (Omnibus var in pasajerosOmnibuss)
-            {   
-               
-                d++;
-                Console.WriteLine("El Omnibus N°{0} lleva {1} pasajero/s", d, var.Pasajeros);
-
-            }
-            Console.ReadLine();
+            
         }
     }
 }
