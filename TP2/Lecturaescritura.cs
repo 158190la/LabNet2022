c5828402ec73bea9192715508b17c89a65c923dd
LautaroDuarte <lautaroduarte1@gmail.com>
2022-06-02 13:38:04 -0300

    Menu

--- TP2/Lecturaescritura.cs
+++ TP2/Lecturaescritura.cs
@@ -18,5 +18,18 @@ namespace TP2
             divisor1 = int.Parse(Console.ReadLine());
             return Tuple.Create(divisor1, dividendo1);
         }
+
+        public static void Menu()
+        {
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Practica - Ingrese su opcion y presione enter para continuar");
+            Console.WriteLine("1-Ejercicio 1");
+            Console.WriteLine("2-Ejercicio 2");
+            Console.WriteLine("3-Ejercicio 3");
+            Console.WriteLine("4-Ejercicio 4");
+            Console.WriteLine("5-Salir");
+            Console.WriteLine("Ingrese su opcion");
+            Console.WriteLine("------------------------------------------------------------");
+        }
     }
 }
--- TP2/Program.cs
+++ TP2/Program.cs
@@ -10,13 +10,7 @@ namespace TP2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practica Excepciones - Ingrese su opcion y presione enter para continuar");
-            Console.WriteLine("1-Ejercicio 1");
-            Console.WriteLine("2-Ejercicio 2");
-            Console.WriteLine("3-Ejercicio 3");
-            Console.WriteLine("4-Ejercicio 4");
-            Console.WriteLine("5-Salir");
-            Console.WriteLine("Ingrese su opcion");
+            Lecturaescritura.Menu();
             int opcion = int.Parse(Console.ReadLine());           
             while (opcion != 5)
             {
@@ -24,50 +18,50 @@ namespace TP2
                 {
                     //ejercicio 1
                     case 1:                        
-                        var divisor1 = Lecturaescritura.IngresoDivisor();                       
-                        try
-                        {
-                            Console.WriteLine(Div0.Dividir(divisor1.Item1,divisor1.Item2));
-                        }
+                            var divisor1 = Lecturaescritura.IngresoDivisor();                       
+                            try
+                            {
+                                Console.WriteLine(Div0.Dividir(divisor1.Item1,divisor1.Item2));
+                            }
                        
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                        finally
-                        {
-                            Console.WriteLine("operacion finalizada");
-                        }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.ToString());
+                            }
+                            finally
+                            {
+                                Console.WriteLine("operacion finalizada");
+                            }
 
                         break;
                     //ejercicio 2
                     case 2:
-                        var divisor2 = Tuple.Create(1,1);
-                        try
-                        {
-                            divisor2 = Lecturaescritura.IngresoDivisor();
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Seguro Ingreso una letra o no ingreso nada");
-                        }                       
+                            var divisor2 = Tuple.Create(1,1);
+                            try
+                            {
+                                divisor2 = Lecturaescritura.IngresoDivisor();
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Seguro Ingreso una letra o no ingreso nada");
+                            }                       
 
-                        try
-                        {
-                            Console.WriteLine(Div0.Dividir(divisor2.Item1, divisor2.Item2));
-                        }
-                        catch (DivideByZeroException)
-                        {
-                            Console.WriteLine("Solo Chuck Norris divide por 0");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                        finally
-                        {
-                            Console.WriteLine("operacion finalizada");
-                        }
+                            try
+                            {
+                                Console.WriteLine(Div0.Dividir(divisor2.Item1, divisor2.Item2));
+                            }
+                            catch (DivideByZeroException)
+                            {
+                                Console.WriteLine("Solo Chuck Norris divide por 0");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.ToString());
+                            }
+                            finally
+                            {
+                                Console.WriteLine("operacion finalizada");
+                            }
                         break;
                     //ejercicio 3
                     case 3: 
@@ -99,15 +93,7 @@ namespace TP2
                         }
                         break;
                 }
-                Console.WriteLine("------------------------------------------------------------");
-                Console.WriteLine("Practica - Ingrese su opcion y presione enter para continuar");
-                Console.WriteLine("1-Ejercicio 1");
-                Console.WriteLine("2-Ejercicio 2");
-                Console.WriteLine("3-Ejercicio 3");
-                Console.WriteLine("4-Ejercicio 4");
-                Console.WriteLine("5-Salir");
-                Console.WriteLine("Ingrese su opcion");
-                Console.WriteLine("------------------------------------------------------------");
+                Lecturaescritura.Menu();
                 opcion = int.Parse(Console.ReadLine());
 
             }
