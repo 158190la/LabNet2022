edadf6ac63f36726cfa61e5d69c40925649b77a2
LautaroDuarte <lautaroduarte1@gmail.com>
2022-06-11 21:15:02 -0300

    BASE

---
+++ tp5/tp5.Logic/CostumerLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using tp5.Entities;
+using tp5.Data;
+
+namespace tp5.Logic
+{
+    public class CustomersLogic : BaseLogic
+    {
+        public List<Customers> GetAll()
+        {
+            return context.Customers.ToList();
+        }
+
+        public void ObjetoCustomer()
+        {
+            var customerBuscado = 0;
+
+            var query = from Customers in context.Customers
+                        where Customers.CustomerID
+                       
+        }
+    }
+}
