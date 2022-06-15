c55e01b130df195e1bcb9c3e7b23edc7eaa3116d
LautaroDuarte <lautaroduarte1@gmail.com>
2022-06-15 17:38:44 -0300

    MVC

---
+++ Lab.Demo.EF/Lab.Demo.MVC/Controllers/RegionController.cs
@@ -0,0 +1,52 @@
+﻿using Lab.Demo.EF.Entities;
+using Lab.Demo.EF.Logic;
+using Lab.Demo.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.Demo.MVC.Controllers
+{
+    public class RegionController : Controller
+    {
+        RegionLogic logic = new RegionLogic();
+        // GET: Region
+        public ActionResult Index()
+        {
+            
+            List<EF.Entities.Region> regiones = logic.GetAll();
+            List<RegionView> regionViews = regiones.Select(s => new RegionView
+            {
+                Id = s.RegionID,
+                Descripcion = s.RegionDescription
+            }).ToList();
+
+
+
+            return View(regionViews);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(RegionView regionView)
+        {
+            try
+            {
+                var regionEntity = new Region { RegionDescription = regionView.Descripcion };
+               
+                logic.Add(regionEntity);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+    }
+}
---
+++ Lab.Demo.EF/Lab.Demo.MVC/Models/RegionView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.Demo.MVC.Models
+{
+    public class RegionView
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+}
