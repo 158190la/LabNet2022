0bbd3100a8800a727e6c0bdef30fad0e73079829
LautaroDuarte <lautaroduarte1@gmail.com>
2022-06-15 23:45:24 -0300

    ADD Delete

---
+++ TP3.EF/TP3.EF.MVC/Controllers/ShippersController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP3.EF.Entities;
+using TP3.EF.Logic;
+using TP3.EF.MVC.Models;
+
+namespace TP3.EF.MVC.Controllers
+{
+    public class ShippersController : Controller
+    {
+        ShippersLogic logic = new ShippersLogic();
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            
+            List<Entities.Shippers> shippers = logic.GetAll();
+            List<ShippersView> shippersViews = shippers.Select(s => new ShippersView
+            {
+                ShipperID = s.ShipperID,
+                CompanyName = s.CompanyName,
+                Phone = s.Phone,
+
+            }).ToList();
+            return View(shippersViews);
+        }
+
+        // GET: Shippers/Details/5
+        public ActionResult Details(int id)
+        {
+            var logic = new ShippersLogic();
+            List<Entities.Shippers> shippers = logic.GetAll();
+            return View(shippers);
+                       
+        }
+
+        // GET: Shippers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shippers/Create
+        [HttpPost]
+        public ActionResult Create(ShippersView shippersViews)
+        {
+            try
+            {
+                Shippers shipperEntity = new Shippers { CompanyName = shippersViews.CompanyName, Phone = shippersViews.Phone};
+
+                logic.Add(shipperEntity);
+              
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index","Error");
+            }
+        }
+
+        // GET: Shippers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Shippers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ShippersView shippersViews)
+        {
+            try
+            {
+                Shippers shipperUpdate = new Shippers { ShipperID = shippersViews.ShipperID };
+                
+                shipperUpdate.CompanyName = shippersViews.CompanyName;
+                shipperUpdate.Phone = shippersViews.Phone;
+                
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shippers/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            logic.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ TP3.EF/TP3.EF.MVC/Models/ShippersView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP3.EF.MVC.Models
+{
+    public class ShippersView
+    {
+        public int ShipperID { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
